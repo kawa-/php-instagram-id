09008ae97cd5b7e7476abe1367514cf83a3557bf
KAWAMORITA Mitsuaki <kawa@hongo.wide.ad.jp>
2014-04-03 16:25:41 +0900

    fix

--- example.php
+++ example.php
@@ -4,6 +4,6 @@ require dirname(__FILE__) . '/InstagramID.php';
 
 $instagram_id = new InstagramID();
 echo 'Instagram ID    : ' . $instagram_id->getID() . "\n";
-echo 'Unix time       : ' . InstagramID::getUnixtime($instagram_id) . "\n";
+echo 'Unix time       : ' . InstagramID::getUnixtime($instagram_id->id) . "\n";
 echo 'time() function : ' . time() . "\n";
 
