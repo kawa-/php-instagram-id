dfb05f5d50fdf4e20d8e915098ebb7b7be2fd92f
KAWAMORITA Mitsuaki <kawa@hongo.wide.ad.jp>
2014-04-22 17:24:56 +0900

    fix for string

--- example.php
+++ example.php
@@ -3,7 +3,8 @@
 require dirname(__FILE__) . '/InstagramID.php';
 
 $instagram_id = new InstagramID();
-echo 'Instagram ID    : ' . $instagram_id->getID() . "\n";
-echo 'Unix time       : ' . InstagramID::getUnixtime($instagram_id->id) . "\n";
-echo 'time() function : ' . time() . "\n";
-
+echo 'Instagram ID (Int)     : ' . $instagram_id->getID() . "\n";
+echo 'Instagram ID (String)  : ' . $instagram_id->getIDByString() . "\n";
+echo 'Unix time (from Int)   : ' . InstagramID::getUnixtime($instagram_id->id) . "\n";
+echo 'Unix time (from String): ' . InstagramID::getUnixtimeFromString($instagram_id->getIDByString()) . "\n";
+echo 'time() function        : ' . time() . "\n";
